542c57e310886bfc2ac24471a6627d2273c956ee
RODRÍGUEZ RAMÍREZ YONATAN ELISEO <2941192024@mail.utec.edu.sv>
2026-05-21 12:06:03 -0600

    Implementación de Interfaz de Estudiante: CRUD de propuestas y filtrado con LINQ
    Creado EstudianteController con lógica de negocio.
    
    Implementada Vista Index para Estudiante (Registro y Listado).
    
    Añadido método GuardarPropuesta en ProyectosDatos.
    
    Aplicado filtrado de proyectos por UsuarioID usando LINQ

--- Models/Proyecto.cs
+++ Models/Proyecto.cs
@@ -9,28 +9,23 @@ namespace PrometeoMVC.Models
     public class Proyecto
     {
         public int ProyectoID { get; set; }
-        public int EstudianteID { get; set; }
-        public int Tipo { get; set; }
+        public int UsuarioID { get; set; } // FK a la tabla Usuarios
 
-        public int EstadoID { get; set; }
-
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string NombreProyecto { get; set; }
 
-        public string Objetivo { get; set; }
-
         [Required(ErrorMessage = "El título es obligatorio")]
-        [Display(Name = "Título del Proyecto")]
         public string TituloProyecto { get; set; }
 
         public string Descripcion { get; set; }
-
-        [Display(Name = "Area de Investigacion")]
+        public string Objetivo { get; set; }
         public int AreaID { get; set; }
+        public string Tipo { get; set; } 
+        public int EstadoID { get; set; }
 
- 
+        // esto para mostrar los nombres en las tabls (JOINs)
         public string Estado { get; set; }
-
-        [Display(Name = "Fecha de Envío")]
+        public string AreaNombre { get; set; }
         public DateTime FechaEnvio { get; set; }
     }
 }
