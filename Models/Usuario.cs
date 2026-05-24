0980bdcb5ade19aa9447d1b1eabdd146bf9391fb
alexromero.si21@gmail.com <alexromero.si21@gmail.com>
2026-05-23 20:51:18 -0600

    Modulo admin completo con login, usuarios y reportes LINQ

---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrometeoMVC.Models
+{
+    public class Usuario
+    {
+
+        public int UsuarioID { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string Rol { get; set; }
+
+    }
+}
